f6c2b2060a762924f9ef7f15fd76c5aa09d5ec99
flax65 <flavio.alberti65@gmail.com>
2026-05-20 13:29:21 +0200

    Add files via upload

---
+++ sw-planner.js
@@ -0,0 +1,29 @@
+const CACHE = 'bmw-planner-v1';
+const ASSETS = [
+    './planner.html',
+    './manifest-planner.json',
+    './icon-192.png',
+    './icon-512.png'
+];
+
+self.addEventListener('install', e => {
+    e.waitUntil(
+        caches.open(CACHE).then(c => c.addAll(ASSETS))
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+    e.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+        )
+    );
+    self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+    e.respondWith(
+        caches.match(e.request).then(cached => cached || fetch(e.request))
+    );
+});
