2177ee878316adc40743196c1de887e14415e9da
flax65 <flavio.alberti65@gmail.com>
2026-05-22 18:46:16 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,36 @@
-// Service worker disabilitato - nessuna cache
-self.addEventListener('install', () => self.skipWaiting());
+const CACHE = 'bmw-dash-v13';
+const ASSETS = [
+    './index.html',
+    './Manifest.json',
+    './icon-192.png',
+    './icon-512.png'
+];
+
+self.addEventListener('install', e => {
+    e.waitUntil(
+        caches.open(CACHE).then(c => c.addAll(ASSETS))
+    );
+    self.skipWaiting();
+});
+
 self.addEventListener('activate', e => {
     e.waitUntil(
-        caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+        caches.keys().then(keys =>
+            Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+        )
     );
     self.clients.claim();
 });
-self.addEventListener('fetch', e => e.respondWith(fetch(e.request)));
+
+// Network-first: carica sempre dalla rete, cache solo se offline
+self.addEventListener('fetch', e => {
+    e.respondWith(
+        fetch(e.request)
+            .then(res => {
+                const clone = res.clone();
+                caches.open(CACHE).then(c => c.put(e.request, clone));
+                return res;
+            })
+            .catch(() => caches.match(e.request))
+    );
+});
